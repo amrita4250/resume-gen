1336f3e67730d311327df9e37dd09affce193bfa
amrita4250 <amritaranjan45054@gmail.com>
2025-01-30 17:27:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+This responsive web application is a great way to create and download resumes in different formats. It provides a user-friendly interface that makes it easy to input details, work experience, education, projects, and more and generate a professional-looking resume in various formats.
+
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+
---
+++ src/components/Body/Body.js
@@ -0,0 +1,108 @@
+import React, { useRef, useState } from "react";
+import ReactToPrint from "react-to-print";
+import { ArrowDown } from "react-feather";
+
+import Editor from "../Editor/Editor";
+import Resume from "../Resume/Resume";
+
+import styles from "./Body.module.css";
+
+function Body() {
+  const colors = ["black", "rgb(4, 102, 4)", "maroon", "white", "rgb(235, 104, 28)", "rgb(5, 15, 63)"];
+  const sections = {
+    basicInfo: "Basic Info",
+    workExp: "Work Experience",
+    project: "Projects",
+    education: "Education",
+    achievement: "Achievements",
+    extracurricular: "Extracurricular",
+    other: "Other",
+  };
+  const resumeRef = useRef();
+
+  const [activeColor, setActiveColor] = useState(colors[0]);
+  const [resumeInformation, setResumeInformation] = useState({
+    [sections.basicInfo]: {
+      id: sections.basicInfo,
+      sectionTitle: sections.basicInfo,
+      detail: {},
+    },
+    [sections.workExp]: {
+      id: sections.workExp,
+      sectionTitle: sections.workExp,
+      details: [],
+    },
+    [sections.project]: {
+      id: sections.project,
+      sectionTitle: sections.project,
+      details: [],
+    },
+    [sections.education]: {
+      id: sections.education,
+      sectionTitle: sections.education,
+      details: [],
+    },
+    [sections.achievement]: {
+      id: sections.achievement,
+      sectionTitle: sections.achievement,
+      points: [],
+    },
+    [sections.extracurricular]: {
+      id: sections.extracurricular,
+      sectionTitle: sections.extracurricular,
+      detail: "",
+    },
+   
+    [sections.other]: {
+      id: sections.other,
+      sectionTitle: sections.other,
+      detail: "",
+    },
+  
+  });
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.heading}><b>Resume Builder</b></p>
+      <div className={styles.toolbar}>
+        <div className={styles.colors}>
+          {colors.map((item) => (
+            <span
+              key={item}
+              style={{ backgroundColor: item }}
+              className={`${styles.color} ${
+                activeColor === item ? styles.active : ""
+              }`}
+              onClick={() => setActiveColor(item)}
+            />
+          ))}
+        </div>
+        <ReactToPrint
+          trigger={() => {
+            return (
+              <button>
+                Download <ArrowDown />
+              </button>
+            );
+          }}
+          content={() => resumeRef.current}
+        />
+      </div>
+      <div className={styles.main}>
+        <Editor
+          sections={sections}
+          information={resumeInformation}
+          setInformation={setResumeInformation}
+        />
+        <Resume
+          ref={resumeRef}
+          sections={sections}
+          information={resumeInformation}
+          activeColor={activeColor}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Body;
